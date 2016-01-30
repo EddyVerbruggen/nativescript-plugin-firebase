625812292034c83f970121a76af18248741299a1
Eddy Verbruggen <eddyverbruggen@gmail.com>
2016-01-30 22:10:51 +0100

    #8 Pass Query to Firebase ref

--- firebase-common.js
+++ firebase-common.js
@@ -1,8 +1,26 @@
 var firebase = {};
 
-firebase.loginType = {
+firebase.LoginType = {
   ANONYMOUS: "anonymous",
   PASSWORD: "password"
+};
+
+firebase.QueryOrderByType = {
+  KEY: "key",
+  VALUE: "value",
+  CHILD: "child",
+  PRIORITY: "priority"    
+};
+
+firebase.QueryLimitType = {
+  FIRST: "first",
+  LAST: "last"
+};
+  
+firebase.QueryRangeType = {
+  START_AT: "startAt",
+  END_AT: "endAt",
+  EQUAL_TO: "equalTo"
 };
 
 firebase.instance = null;
