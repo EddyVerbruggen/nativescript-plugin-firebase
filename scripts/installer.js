3b27bfc2a23150166917c22e1ced504636733aba
EddyVerbruggen <eddyverbruggen@gmail.com>
2016-11-20 21:32:37 +0100

    Some automations for Android #198

--- scripts/installer.js
+++ scripts/installer.js
@@ -138,6 +138,7 @@ function promptQuestionsResult(result) {
     if(usingAndroid) {
         writeGradleFile(result);
         writeGoogleServiceCopyHook();
+        writeGoogleServiceGradleHook();
     }
     console.log('Firebase post install completed. To re-run this script, navigate to the root directory of `nativescript-plugin-firebase` in your `node_modules` folder and run: `npm run config`.');
 }
@@ -311,6 +312,56 @@ module.exports = function() {
         fs.writeFileSync(scriptPath, scriptContent);
     } catch(e) {
         console.log("Failed to install google-service.json copy hook.");
+        console.log(e);
+    }
+}
+
+function writeGoogleServiceGradleHook() {
+    console.log("Install firebase-build-gradle hook.");
+    try {
+        var scriptContent =
+`
+var path = require("path");
+var fs = require("fs");
+
+module.exports = function() {
+
+    console.log("Configure firebase");
+    var buildGradlePath = path.join(__dirname, "..", "..", "platforms", "android", "build.gradle"); 
+    if (fs.existsSync(buildGradlePath)) {
+        var buildGradleContent = fs.readFileSync(buildGradlePath).toString();
+
+        if (buildGradleContent.indexOf('classpath "com.google.gms:google-services:3.0.0"') != -1) {
+            return;
+        }
+
+        var search = -1;
+
+        search = buildGradleContent.indexOf("repositories", 0);
+        if (search == -1) {
+            return;
+        }
+
+        search = buildGradleContent.indexOf("dependencies", search);
+        if (search == -1) {
+            return;
+        }
+
+        search = buildGradleContent.indexOf("}", search);
+        if (search == -1) {
+            return;
+        }
+
+        buildGradleContent = buildGradleContent.substr(0, search - 1) + '    classpath "com.google.gms:google-services:3.0.0"\\n    ' + buildGradleContent.substr(search - 1);
+
+        fs.writeFileSync(buildGradlePath, buildGradleContent);
+    }
+};
+`;
+        var scriptPath = path.join(appRoot, "hooks", "after-prepare", "firebase-build-gradle.js");
+        fs.writeFileSync(scriptPath, scriptContent);
+    } catch(e) {
+        console.log("Failed to install firebase-build-gradle hook.");
         console.log(e);
     }
 }
