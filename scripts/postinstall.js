6d1c06d4de460571943bd3e9969ddc835dca32a7
EddyVerbruggen <eddyverbruggen@gmail.com>
2017-06-20 19:28:21 +0200

    Podfile wasn't updating correctly

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3013,7 +3013,7 @@ function writePodFile(result) {
     }
     try {
         fs.writeFileSync(directories.ios + '/Podfile',
-`pod 'Firebase', '~> 3.17.0'
+`pod 'Firebase', '~> 4.0.2'
 pod 'Firebase/Database'
 pod 'Firebase/Auth'
 
