b2a34d1d904cda3ed6870c833b17ce76e198669e
EddyVerbruggen <eddyverbruggen@gmail.com>
2018-04-17 16:58:45 +0200

    NativeScript 4 bump

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -4,4 +4,4 @@ import * as application from 'tns-core-modules/application';
 // added this here so we can do some wiring
 require("nativescript-plugin-firebase");
 
-application.start({ moduleName: "main-page" });
+application.run({ moduleName: "main-page" });
