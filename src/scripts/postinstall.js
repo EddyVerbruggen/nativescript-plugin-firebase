dbf48b881450fd3b4b75e2a9182e6f895f59001b
Bart van den Burg <bart.vandenburg@neocles.io>
2018-05-21 17:10:57 +0200

    Tell user to read the documentation if google-services.json does not exist

--- src/scripts/postinstall.js
+++ src/scripts/postinstall.js
@@ -3422,17 +3422,21 @@ module.exports = function($logger, $projectData, hookArgs) {
         if (hookArgs.platform.toLowerCase() === 'android') {
             var sourceGoogleJson = path.join($projectData.appResourcesDirectoryPath, "Android", "google-services.json");
             var destinationGoogleJson = path.join($projectData.platformsDir, "android", "app", "google-services.json");
-            if (fs.existsSync(sourceGoogleJson) && fs.existsSync(path.dirname(destinationGoogleJson))) {
-                $logger.out("Copy " + sourceGoogleJson + " to " + destinationGoogleJson + ".");
-                fs.writeFileSync(destinationGoogleJson, fs.readFileSync(sourceGoogleJson));
-                resolve()
-            } else {
-                $logger.warn("Unable to copy google-services.json.");
-                reject();
+            if (!fs.existsSync(sourceGoogleJson)) {
+                $logger.warn(sourceGoogleJson + " does not exist. Please follow the installation instructions from the documentation");
+                return reject();
             }
-        } else {
-            resolve()
+            
+            if (!fs.existsSync(path.dirname(destinationGoogleJson))) {
+                $logger.warn("Unable to copy google-services.json.");
+                return reject();
+            }
+
+            $logger.out("Copy " + sourceGoogleJson + " to " + destinationGoogleJson + ".");
+            fs.writeFileSync(destinationGoogleJson, fs.readFileSync(sourceGoogleJson));
         }
+
+        resolve()
     });
 };
 `;
