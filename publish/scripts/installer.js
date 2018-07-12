e274cd5eef7613f7744324bbe7324f8d0d30ef3c
EddyVerbruggen <eddyverbruggen@gmail.com>
2018-07-12 21:50:23 +0200

    Unable to copy google-services.json. #701

--- publish/scripts/installer.js
+++ publish/scripts/installer.js
@@ -624,9 +624,15 @@ module.exports = function($logger, $projectData, hookArgs) {
         if (hookArgs.platform.toLowerCase() === 'android') {
             var sourceGoogleJson = path.join($projectData.appResourcesDirectoryPath, "Android", "google-services.json");
             var destinationGoogleJson = path.join($projectData.platformsDir, "android", "app", "google-services.json");
+            var destinationGoogleJsonAlt = path.join($projectData.platformsDir, "android", "google-services.json");
             if (fs.existsSync(sourceGoogleJson) && fs.existsSync(path.dirname(destinationGoogleJson))) {
                 $logger.out("Copy " + sourceGoogleJson + " to " + destinationGoogleJson + ".");
                 fs.writeFileSync(destinationGoogleJson, fs.readFileSync(sourceGoogleJson));
+                resolve();
+            } else if (fs.existsSync(sourceGoogleJson) && fs.existsSync(path.dirname(destinationGoogleJsonAlt))) {
+                // NativeScript < 4 doesn't have the 'app' folder
+                $logger.out("Copy " + sourceGoogleJson + " to " + destinationGoogleJsonAlt + ".");
+                fs.writeFileSync(destinationGoogleJsonAlt, fs.readFileSync(sourceGoogleJson));
                 resolve();
             } else {
                 $logger.warn("Unable to copy google-services.json.");
