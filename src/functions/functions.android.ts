51b1709d2fb4deb2c5d858879e86262b1256c745
Brendan Ingham <b.ingham@spartadigital.co.uk>
2018-09-22 21:23:04 +0100

    Android Function complete, needs tests

--- src/functions/functions.android.ts
+++ src/functions/functions.android.ts
@@ -3,8 +3,10 @@ export function httpsCallable<I = {}, O = {}>(functionName: string) {
 
     return (data: I) => new Promise<O>((resolve, reject) => {
 
+        let actData = convertToNative(data);
+
         return instance.getHttpsCallable(functionName)
-                .call(data)
+                .call(actData)
                 .continueWith(new (<any>com.google.android.gms).tasks.Continuation({
                     then: (task) => {
 
@@ -23,4 +25,27 @@ export function httpsCallable<I = {}, O = {}>(functionName: string) {
     });
 
 
+}
+
+function convertToNative(input: any) {
+
+    if ( typeof input === "string" ) {
+        return new java.lang.String(input);
+    } else if (typeof input === "number") {
+        if ( input % 1 === 0) {
+            return new java.lang.Integer(input);
+        } else {
+            return new java.lang.Float(input);
+        }
+    } else if ( typeof input === "boolean" ) {
+        return new java.lang.Boolean(input);
+    } else if ( Array.isArray(input) ) {
+        const hashset = new java.util.ArrayList();
+        input.forEach( (currentItem) => hashset.add(convertToNative(currentItem)));
+        return hashset;
+    } else {
+        const hashmap = new java.util.HashMap<string, any>();
+        Object.keys(input).forEach((currentItem ) => hashmap.put( currentItem, convertToNative(input[currentItem])));
+        return hashmap;
+    }
 }
