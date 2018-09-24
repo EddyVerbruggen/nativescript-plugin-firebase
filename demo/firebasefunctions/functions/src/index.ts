5a7414a9927bf991951db49479a7c6fd46d18d06
EddyVerbruggen <eddyverbruggen@gmail.com>
2018-09-24 18:58:41 +0200

    Support for Firebase 'Callable' functions #806 (added a proper callable function)

--- demo/firebasefunctions/functions/src/index.ts
+++ demo/firebasefunctions/functions/src/index.ts
@@ -6,3 +6,14 @@ import * as functions from "firebase-functions";
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
+
+export const helloWorldJson = functions.https.onRequest((request, response) => {
+  response.json({"message": "Hello from Firebase!"});
+});
+
+// this is a proper callable function
+export const helloName = functions.https.onCall((data, context) => {
+  return {
+    message: "Hello, " + data
+  }
+});
