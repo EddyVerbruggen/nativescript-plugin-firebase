d36d41ca6c2cf035b7a9b9ad954d4162571e984f
Kevin Li <kevin.li@onsip.com>
2019-01-16 13:46:12 -0500

    [query] - Add webQueryInterface to implement database queries through webapi

--- src/firebase.d.ts
+++ src/firebase.d.ts
@@ -546,6 +546,23 @@ export interface OnDisconnect {
 
   update(values: Object): Promise<any>;
 }
+// WebAPI Query
+export interface Query {
+  on(eventType: string, callback: (a: any, b?: string) => any): Promise<any>;
+  once(eventType: string): Promise<any>;
+  off(eventType?: string, callback?: (a: DataSnapshot, b?: string | null) => any, context?: Object | null): void;
+  orderByChild(value: string): Query;
+  orderByKey(): Query;
+  orderByPriority(): Query;
+  orderByValue(): Query;
+  equalTo(value: string | number | boolean, key?: string): Query;
+  startAt(value: string | number | boolean, key?: string): Query;
+  endAt(value: string | number | boolean, key?: string): Query;
+  limitToFirst(value: number): Query;
+  limitToLast(value: number): Query;
+}
+
+export function webQuery(path: string): Query;
 
 export interface DataSnapshot {
   key: string;
