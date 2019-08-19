fe5c0de8522b735e45401415b77139f5edc2086f
EddyVerbruggen <eddyverbruggen@gmail.com>
2019-08-19 21:34:09 +0200

    Add support for In-App Messaging event triggers #1285

--- src/functions/functions.d.ts
+++ src/functions/functions.d.ts
@@ -1,2 +1,3 @@
-export declare type HttpsCallable<I, O> = ( callableData: I ) => Promise<O>;
+export declare type HttpsCallable<I, O> = (callableData: I) => Promise<O>;
+
 export declare function httpsCallable<I = {}, O = {}>(functionName: string): HttpsCallable<I, O>;
