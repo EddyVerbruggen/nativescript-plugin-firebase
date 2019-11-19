e342c4bb5c90ccdc5512d3156974e0c4c60027bf
EddyVerbruggen <eddyverbruggen@gmail.com>
2019-11-19 19:35:14 +0100

    Functions++

---
+++ demo/firebasefunctions/functions/README.md
@@ -0,0 +1,12 @@
+## Testing your functions locally
+> This will also do a `tsc`
+
+$ npm run serve
+
+## Debugging your functions locally
+> This will also do a `tsc`
+
+$ npm run debug
+
+## To deploy a new version
+$ firebase deploy --only functions
--- demo/firebasefunctions/functions/src/index.ts
+++ demo/firebasefunctions/functions/src/index.ts
@@ -1,19 +1,35 @@
 import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
 export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+  response.send(`Hello from Firebase!!! ${request.query.text}`);
 });
 
 export const helloWorldJson = functions.https.onRequest((request, response) => {
   response.json({"message": "Hello from Firebase!"});
 });
 
-// this is a proper callable function
+// this is a callable (from code) function
 export const helloName = functions.https.onCall((data, context) => {
   return {
-    message: "Hello: " + data
+    message: `Hello: ${data}`
   }
 });
+
+
+/*
+// Examples of scheduled functions (these require at least the 'Blaze' plan)
+
+exports.scheduledFunction = functions.pubsub.schedule('every 2 minutes').onRun((context) => {
+  console.log('This will be run every 2 minutes!');
+  return null;
+});
+
+exports.scheduledFunctionCrontab = functions.pubsub.schedule('5 11 * * *')
+  .timeZone('America/New_York') // Users can choose timezone - default is America/Los_Angeles
+  .onRun((context) => {
+    console.log('This will be run every day at 11:05 AM Eastern!');
+    return null;
+  }
+);
+
+*/
